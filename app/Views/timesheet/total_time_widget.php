b854079b32a3ceed310e70d17d0216519d003923
harshald360 <harsh.zeplinix.com@gmail.com>
2024-06-07 20:36:37 +0530

    General info in team members design changes

--- app/Views/timesheet/total_time_widget.php
+++ app/Views/timesheet/total_time_widget.php
@@ -3,8 +3,8 @@
     <?php if ($show_total_hours_worked) { ?>
         <div class="box-content <?php echo $show_total_project_hours ? 'border-end' : ''; ?>">
             <div class="card-body ">
-                <h1><?php echo $total_hours_worked; ?></h1>
-                <span class="text-off uppercase"><?php echo app_lang("total_hours_worked"); ?></span>
+                <h3 class="open-time-heading a" ><?php echo $total_hours_worked; ?></h3>
+                <span class="profile-name fw-bold "><?php echo app_lang("total_hours_worked"); ?></span>
             </div>
         </div>
     <?php } ?>
@@ -12,8 +12,8 @@
     <?php if ($show_total_project_hours) { ?>
         <div class="box-content">
             <div class="card-body ">
-                <h1 class=""><?php echo $total_project_hours; ?></h1>
-                <span class="text-off uppercase"><?php echo app_lang("total_project_hours"); ?></span>
+                <h3 class="open-time-heading a"><?php echo $total_project_hours; ?></h3>
+                <span class="profile-name fw-bold "><?php echo app_lang("total_project_hours"); ?></span>
             </div>
         </div>
     <?php } ?>
